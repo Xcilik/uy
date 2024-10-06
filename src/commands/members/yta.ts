a512a6329b5fb7fb63b9d2a6971d68ebd3a50921
Xcilik <149920327+Xcilik@users.noreply.github.com>
2024-10-06 17:38:47 +0700

    Update yta.ts

--- src/commands/members/yta.ts
+++ src/commands/members/yta.ts
@@ -1,12 +1,3 @@
-import { WAMessage } from "@whiskeysockets/baileys";
-import ytdl from "ytdl-core";
-import fs from "fs";
-import { MsgInfoObj } from "../../interfaces/msgInfoObj";
-import { Bot } from "../../interfaces/Bot";
-
-import getRandomFileName from "../../functions/getRandomFileName";
-import { prefix } from "../../utils/constants";
-
 const handler = async (bot: Bot, msg: WAMessage, msgInfoObj: MsgInfoObj) => {
   const { reply, args, from } = msgInfoObj;
   if (args.length === 0) {
@@ -27,11 +18,22 @@ const handler = async (bot: Bot, msg: WAMessage, msgInfoObj: MsgInfoObj) => {
   const titleYt = infoYt.videoDetails.title;
   const randomFileName = getRandomFileName(".mp3");
 
+  const cs = 'VISITOR_INFO1_LIVE=BZLnInSfl8c; VISITOR_PRIVACY_METADATA=CgJJRBIEGgAgTQ%3D%3D; YSC=M1JMuaGd1bY; GPS=1; __Secure-1PSIDTS=sidts-CjIBQlrA-AoaXSzeu_estzOOqKcIKs_dg3IsTrIKWdcM_0jI0POp4qYxVed4J2DO6tTc0RAA; __Secure-3PSIDTS=sidts-CjIBQlrA-AoaXSzeu_estzOOqKcIKs_dg3IsTrIKWdcM_0jI0POp4qYxVed4J2DO6tTc0RAA; __Secure-3PAPISID=mefVImErXPpxDKIx/AintujD7iw4LIvc7L; __Secure-3PSID=g.a000owgr1USCR4MbzaX3jVTew_8TypWJ67cVoNqC1UZ2zU53yBBnCruEZdRhmGOIBswL9DK94wACgYKAYUSARISFQHGX2Miv5hfuP6B0_wLsZxXD3PuYhoVAUF8yKoNuFnJoMl0Cj3AjZJJ299w0076; LOGIN_INFO=AFmmF2swRAIgbMjcT_ZWyfcNTh50rMFpjahnJmiA3xk6wlkNnRHbYDoCICPwMpvbnWPttyHQ95SJ1Apspm3gB7Z3ui1lFVnpWSAb:QUQ3MjNmdzRTdEtMbWRtbTFDNVdvbnJNbXUxNnlwVFliZHFiMEFFN0hfWXZEb2xwb3FkdGJWajM0T0wxdmlRa0xZU2hsaUhjNTVLcG84c19BckNsY0VUVUFQZVVZZGRuZUNzc05yN1RsY3F1bXlIWnMzM05ZVmNKM3Z3dXVfVXVTbUxRLVZlODk4anFnbTlVUG16aWhSNThsYUNFYTNHdkVB; PREF=f6=40000000&tz=Asia.Jakarta&f7=100; __Secure-3PSIDCC=AKEyXzXdi-qvela_G6HmCGp7EfAhBHpO_2cp2qOMyM45F8DWhyC5ZzfXvzUPd-cq9dp-Vh2poQ'; // replace with your cookie value
+  const authorization = 'SAPISIDHASH 1728210575_15fa4e74ca9a6dfdbf48eb50b8ef016adfd87078_u'; // replace with your authorization value
+
   const stream = ytdl(urlYt, {
+    quality: "highestaudio",
+    requestOptions: {
+      headers: {
+        Cookie: cs,
+        Authorization: authorization,
+        "x-youtube-identity-token",
+      },
+    },
     filter: (info) => info.audioBitrate === 160 || info.audioBitrate === 128,
   }).pipe(fs.createWriteStream(`./${randomFileName}`));
+
   console.log("Audio downloading ->", urlYt);
-  // await reply("Downloading.. This may take upto 5 min!");
   await new Promise((resolve, reject) => {
     stream.on("error", reject);
     stream.on("finish", resolve);
@@ -39,9 +41,9 @@ const handler = async (bot: Bot, msg: WAMessage, msgInfoObj: MsgInfoObj) => {
 
   const stats = fs.statSync(`./${randomFileName}`);
   const fileSizeInBytes = stats.size;
-  // Convert the file size to megabytes (optional)
   const fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
-  console.log(`Audio downloaded ! Size: ${fileSizeInMegabytes}`);
+  console.log(`Audio downloaded! Size: ${fileSizeInMegabytes}`);
+
   if (fileSizeInMegabytes <= 100) {
     await bot.sendMessage(
       from,
@@ -53,16 +55,8 @@ const handler = async (bot: Bot, msg: WAMessage, msgInfoObj: MsgInfoObj) => {
       { quoted: msg }
     );
   } else {
-    await reply(`❌ Cannot download! Audio size limit: 100 mb`);
+    await reply(`❌ Cannot download! Audio size limit: 100 MB`);
   }
 
   fs.unlinkSync(`./${randomFileName}`);
 };
-
-const yta = () => {
-  const cmd = ["yta"];
-
-  return { cmd, handler };
-};
-
-export default yta;
